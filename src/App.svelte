469f6c433d2999c5e90e2be079bfae588b7083fb
Silvio Giannini <giannini.silvio@gmail.com>
2020-01-31 18:59:25 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,327 @@
+<script>
+	let subject, from, body;
+
+	function resetForm() {
+		subject = "";
+		from = "";
+		body = "";
+	}
+
+	async function sendMail() {
+		
+		const payload = 
+			 {
+					from: from,
+					subject: subject,
+					body: body
+				};
+	
+		console.log("invio mail: ", payload);
+		let response = await fetch("https://europe-west1-fir-test-fb934.cloudfunctions.net/message", {
+  			method: "POST",
+				headers: {
+						'Accept': 'application/json, text/plain, */*',
+						'Content-Type': 'application/json'
+				},
+  			body: JSON.stringify(payload)
+		});
+
+		console.log("invio mail response : " + response);
+	}
+</script>
+
+<main>
+<div class="background">
+  <div class="contact-form-container">
+    <div class="screen">
+      <div class="screen-header">
+        <div class="screen-header-left">
+          <div class="screen-header-button close"></div>
+          <div class="screen-header-button maximize"></div>
+          <div class="screen-header-button minimize"></div>
+        </div>
+        <div class="screen-header-right">
+          <div class="screen-header-ellipsis"></div>
+          <div class="screen-header-ellipsis"></div>
+          <div class="screen-header-ellipsis"></div>
+        </div>
+      </div>
+      <div class="screen-body">
+        <div class="screen-body-item left">
+          <div class="app-title">
+            <span>CONTACT</span>
+            <span>ME</span>
+          </div>
+          <div class="app-contact"></div>
+        </div>
+        <div class="screen-body-item">
+          <div class="app-form">
+						<div class="app-form-group">
+              <input class="app-form-control" placeholder="Email" bind:value={from}>
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="Subject" bind:value={subject}>
+            </div>
+            <div class="app-form-group message">
+              <textarea  class="app-form-control" placeholder="Message" bind:value={body}></textarea>
+            </div>
+            <div class="app-form-group buttons">
+              <button class="app-form-button" on:click={resetForm}>Reset</button>
+              <button class="app-form-button" on:click={sendMail}>Send</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</main>
+
+
+<style>
+*, *:before, *:after {
+  box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+body {
+  background: linear-gradient(to right, #ea1d6f 0%, #eb466b 100%);
+  font-size: 12px;
+}
+
+body, button, input, textarea {
+  font-weight: 700;
+  letter-spacing: 1.4px;
+}
+
+.background {
+  display: flex;
+  min-height: 50vh;
+}
+
+.contact-form-container {
+  flex: 0 1 700px;
+  margin: auto;
+  padding: 10px;
+}
+
+.screen {
+  position: relative;
+  background: #3e3e3e;
+  border-radius: 15px;
+}
+
+.screen:after {
+  content: '';
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 20px;
+  right: 20px;
+  bottom: 0;
+  border-radius: 15px;
+  box-shadow: 0 20px 40px rgba(0, 0, 0, .4);
+  z-index: -1;
+}
+
+.screen-header {
+  display: flex;
+  align-items: center;
+  padding: 10px 20px;
+  background: #4d4d4f;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+}
+
+.screen-header-left {
+  margin-right: auto;
+}
+
+.screen-header-button {
+  display: inline-block;
+  width: 8px;
+  height: 8px;
+  margin-right: 3px;
+  border-radius: 8px;
+  background: white;
+}
+
+.screen-header-button.close {
+  background: #ed1c6f;
+}
+
+.screen-header-button.maximize {
+  background: #e8e925;
+}
+
+.screen-header-button.minimize {
+  background: #74c54f;
+}
+
+.screen-header-right {
+  display: flex;
+}
+
+.screen-header-ellipsis {
+  width: 3px;
+  height: 3px;
+  margin-left: 2px;
+  border-radius: 8px;
+  background: #999;
+}
+
+.screen-body {
+  display: flex;
+}
+
+.screen-body-item {
+  flex: 1;
+  padding: 50px;
+}
+
+.screen-body-item.left {
+  display: flex;
+  flex-direction: column;
+}
+
+.app-title {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  color: #ea1d6f;
+  font-size: 26px;
+}
+
+.app-title:after {
+  content: '';
+  display: block;
+  position: absolute;
+  left: 0;
+  bottom: -10px;
+  width: 25px;
+  height: 4px;
+  background: #ea1d6f;
+}
+
+.app-contact {
+  margin-top: auto;
+  font-size: 8px;
+  color: #888;
+}
+
+.app-form-group {
+  margin-bottom: 15px;
+}
+
+.app-form-group.message {
+  margin-top: 40px;
+}
+
+.app-form-group.buttons {
+  margin-bottom: 0;
+  text-align: right;
+}
+
+.app-form-control {
+  width: 100%;
+  padding: 10px 0;
+  background: none;
+  border: none;
+  border-bottom: 1px solid #666;
+  color: #ddd;
+  font-size: 14px;
+  outline: none;
+  transition: border-color .2s;
+}
+
+.app-form-control::placeholder {
+  color: #666;
+}
+
+textarea {
+    padding: 30px;
+    border: 0;
+    width: 100%;
+    font-size: 1rem;
+    background-color: #2d2d2d;
+    color: white;
+    border-radius: 4px;
+}
+
+.app-form-control:focus {
+  border-bottom-color: #ddd;
+}
+
+.app-form-button {
+  background: none;
+	display: inline-flex;
+  border: none;
+  color: #ea1d6f;
+  font-size: 14px;
+  cursor: pointer;
+  outline: none;
+}
+
+.app-form-button:hover {
+  color: #b9134f;
+}
+
+.credits {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+  color: #ffa4bd;
+  font-size: 16px;
+  font-weight: normal;
+}
+
+.credits-link {
+  display: flex;
+  align-items: center;
+  color: #fff;
+  font-weight: bold;
+  text-decoration: none;
+}
+
+.dribbble {
+  width: 20px;
+  height: 20px;
+  margin: 0 5px;
+}
+
+@media screen and (max-width: 520px) {
+  .screen-body {
+    flex-direction: column;
+  }
+
+  .screen-body-item.left {
+    margin-bottom: 30px;
+  }
+
+  .app-title {
+    flex-direction: row;
+  }
+
+  .app-title span {
+    margin-right: 12px;
+  }
+
+  .app-title:after {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .screen-body {
+    padding: 40px;
+  }
+
+  .screen-body-item {
+    padding: 0;
+  }
+}
+
+</style>
---
+++ src/styles.js
@@ -0,0 +1,31 @@
+import emotion from 'emotion/dist/emotion.umd.min.js';
+
+const { css } = emotion;
+
+const brand = '#74D900';
+
+export const title = css`
+	color: ${brand};
+	font-size: 1em;
+	white-space: nowrap;
+`;
+
+export const box = css`
+	position: relative;
+	display: inline-block;
+	border: 2px solid ${brand};
+	line-height: 1;
+	padding: 4px;
+	border-radius: 4px;
+`;
+
+export const link = css`
+	color: inherit;
+	font-weight: bold;
+	text-decoration: none;
+	border-bottom: 1px solid ${brand};
+	&:hover {
+		text-decoration: none;
+		background: ${brand};
+	}
+`;
